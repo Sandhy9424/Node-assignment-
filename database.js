c5673d459c6c1a9a05b116816ec89e8afe050804
Sandhy9424 <116244806+Sandhy9424@users.noreply.github.com>
2023-11-28 01:48:47 +0530

    Update database.js

--- database.js
+++ database.js
@@ -6,6 +6,10 @@ const pool=new Pool({
     host:"database-1.cwdtoy3mxidy.eu-north-1.rds.amazonaws.com",
     database:"books",
     port:5432,
+    ssl: {
+          require: true, // This will help you. But you will see nwe error
+          rejectUnauthorized: false // This line will fix new error
+        }
 })
 
 module.exports=pool;
